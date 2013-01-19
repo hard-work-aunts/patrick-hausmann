6443a0c8d07b46276a75bc4513ba65a391d82ebe
Patrick Hausmann <patrick.hausmann@covimo.de>
2013-01-19 14:25:22 +0100

    update the code  - reorder  - add density plot for 'lon' / 'lat' by foodchain  - add 'theme_bw' for plot  - add density map

--- R/01-Import.R
+++ R/01-Import.R
@@ -13,10 +13,36 @@
 ##===========================================================================
 
 library(rgdal)
+library(ggplot2)
 
 options(stringsAsFactors = FALSE) 
 
 setwd("E:/Privat/pjgithub/dataexpedition/patrick-hausmann")
+
+Read_Data <- function(m) {
+    k <- list()
+    for (i in seq_along(m)) {
+        xfile  <- paste(m[i], ".csv", sep="")
+        k[[i]] <- read.csv(file.path("Raw Data", xfile))
+        k[[i]]$firm <- m[i]
+    }
+    names(k) <- m
+    return(k)
+}
+
+Plot_Density <- function(x, xvar) {
+    m <- ggplot(x, aes_string(x=xvar, colour="firm", group="firm"))
+    m + geom_density(fill=NA, size=1) + theme_bw()
+}
+
+Check_if_File_exists <- function(x) {
+    outfile <- file.path("data", paste(names(x), ".geojson", sep=""))
+     for (i in seq_along(outfile)) {
+        if (file.exists(outfile[i])) {
+            file.remove(outfile[i])
+        }
+    }
+}
 
 Write_GeoJSON <- function(x) {
 	# write data as geojson	
@@ -26,16 +52,7 @@ Write_GeoJSON <- function(x) {
 	writeOGR(x, outfile, "x", driver="GeoJSON")
 }
 
-Read_Data <- function(m) {
-	k <- list()
-	for (i in seq_along(m)) {
-		xfile  <- paste(m[i], ".csv", sep="")
-		k[[i]] <- read.csv(file.path("Raw Data", xfile))
-		k[[i]]$firm <- m[i]
-	}
-	names(k) <- m
-	return(k)
-}
+###################################################################
 
 m <- c("starbucks", "dunkin_donuts", "kfc", "mcdonalds", 
 		   "pizza_hut", "taco_bell")
@@ -75,19 +92,22 @@ table(x$firm)
 tapply(x$latitude, x$firm, fivenum)
 tapply(x$longitude, x$firm, fivenum)
 
-plot(density(x$longitude))
-plot(density(x$latitude))
+pdf("graphics/Density_lon_lat.pdf", width=11.69, height=8.27)
+    Plot_Density(x, "longitude")
+    Plot_Density(x, "latitude")
+dev.off()
+
+pdf("graphics/Density_map.pdf", width=11.69, height=8.27)
+    x1 <- transform(x, long = round(longitude, 2), lat = round(latitude, 2))
+    p1 <- ggplot(x1, aes (x=long, y=lat, fill = firm)) + theme_bw()
+    p1 <- p1 + stat_binhex (bins=50, aes (alpha = ..count..))
+    p2 <- p1 + facet_wrap(~ firm)
+    print(p1)
+    print(p2)
+    rm(x1, p1, p2)
+dev.off()
 
 xs <- split(x, x$firm)
-
-Check_if_File_exists <- function(x) {
-	outfile <- file.path("data", paste(names(x), ".geojson", sep=""))
-     for (i in seq_along(outfile)) {
-     	if (file.exists(outfile[i])) {
-       		file.remove(outfile[i])
-     	}
-  	}
-}
 
 Check_if_File_exists(xs)
 
